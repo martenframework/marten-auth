51bd344cb499ae4a928d50e9c84216b29975376e
Morgan Aubert <morgan@aubert.email>
2023-01-28 16:26:10 -0500

    Update ameba configuration

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -15,28 +15,29 @@ require "marten/spec"
 {% end %}
 
 require "../src/marten_auth"
+require "../src/marten_auth/spec"
 
 require "./test_project"
 
-def for_mysql(&block)
+def for_mysql(&)
   for_db_backends(:mysql) do
     yield
   end
 end
 
-def for_postgresql(&block)
+def for_postgresql(&)
   for_db_backends(:postgresql) do
     yield
   end
 end
 
-def for_sqlite(&block)
+def for_sqlite(&)
   for_db_backends(:sqlite) do
     yield
   end
 end
 
-def for_db_backends(*backends : String | Symbol, &block)
+def for_db_backends(*backends : String | Symbol, &)
   current_db_backend = ENV["MARTEN_SPEC_DB_CONNECTION"]? || "sqlite"
   if backends.map(&.to_s).includes?(current_db_backend)
     yield
