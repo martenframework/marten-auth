768186a941bdf8ddf871931f45754734adf92403
Morgan Aubert <morgan@aubert.email>
2025-12-29 10:20:15 -0400

    Fix typo

--- src/marten_auth/middleware.cr
+++ src/marten_auth/middleware.cr
@@ -2,7 +2,7 @@ module MartenAuth
   # The authentication middleware.
   #
   # This middleware ensures that the currently authenticated user ID is associated to the current request. This ensures
-  # that this user can be easily retrived later on.
+  # that this user can be easily retrieved later on.
   class Middleware < Marten::Middleware
     def call(request : Marten::HTTP::Request, get_response : Proc(Marten::HTTP::Response)) : Marten::HTTP::Response
       request.user_id = MartenAuth.get_user_session_key(request)
